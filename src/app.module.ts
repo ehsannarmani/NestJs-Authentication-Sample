db2c4aa1dc47749d3ffb35cf6e1754a406b60231
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2024-08-12 13:20:28 +0330

    change configuration and add env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,24 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./user.entity";
 import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'nestjs',
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
@@ -23,6 +29,9 @@ import { JwtModule } from "@nestjs/jwt";
       signOptions: {
         expiresIn: '365d'
       }
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
     })
   ],
   controllers: [UserController],
